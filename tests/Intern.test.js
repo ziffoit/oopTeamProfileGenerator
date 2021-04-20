59481a82f73a0039669fb8835f64f7c5b3074db2
Kevin Hanson <ziffoit@gmail.com>
2021-04-19 20:38:39 -0600

    Overwrite employee

---
+++ tests/Intern.test.js
@@ -0,0 +1,5 @@
+const { expect, test } = require("@jest/globals")
+
+test("", () =>  {
+    expect(true).toBe(true)
+})
---
+++ tests/Manager.test.js
@@ -0,0 +1,5 @@
+const { expect } = require("@jest/globals")
+
+test("", () =>  {
+    expect(true).toBe(true)
+})
