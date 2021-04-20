e6407d39b1c363265502fe7c4817b9b2c76916a2
Kevin Hanson <ziffoit@gmail.com>
2021-04-20 14:28:09 -0600

    Manager test

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,5 +1,13 @@
 const { expect } = require("@jest/globals")
+const Manager = require("../lib/Manager")
 
-test("", () =>  {
-    expect(true).toBe(true)
+test("Update role with Manager.", () => {
+    const E = new Manager("name", "id", "email", "officeNumber")
+    expect(E.getRole()).toBe('Manager')
+})
+
+test("Can set an officeNumber in the constructor.", () => {
+    const officeNumber = "5"
+    const E = new Manager("name", "id", "email", officeNumber)
+    expect(E.getOfficeNumber()).toBe(officeNumber)
 })
