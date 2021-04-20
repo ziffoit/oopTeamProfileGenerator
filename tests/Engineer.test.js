6e75a228b6db2420e47e47df4db1eee53f4e61b3
Kevin Hanson <ziffoit@gmail.com>
2021-04-19 19:51:30 -0600

    Engineer role test

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -5,3 +5,9 @@ test("Update role with Engineer.", () => {
     const E = new Engineer("name", "id", "email", "github")
     expect(E.getRole()).toBe('Engineer')
 })
+
+// test("Can set a name in the constructor.", () => {
+//     const name = "Kevin"
+//     const E = new Employee(name, "id", "email")
+//     expect(E.getName()).toBe(name)
+// })
