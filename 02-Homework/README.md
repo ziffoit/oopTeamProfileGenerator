3bbda962ba48b1725b7f397e2905716030698035
Kevin Hanson <ziffoit@gmail.com>
2021-04-25 15:23:53 -0600

    info functions

--- 02-Homework/README.md
+++ 02-Homework/README.md
@@ -23,9 +23,9 @@ GIVEN a command-line application that accepts user input
 WHEN I am prompted for my team members and their information
 THEN an HTML file is generated that displays a nicely formatted team roster based on user input
 WHEN I click on an email address in the HTML
-THEN my default email program opens and populates the TO field of the email with the address
+THEN my default email program opens and populates the TO field of the email with the address //mailto: in html link
 WHEN I click on the GitHub username
-THEN that GitHub profile opens in a new tab
+THEN that GitHub profile opens in a new tab //target _blank
 WHEN I start the application
 THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
 WHEN I enter the team manager’s name, employee ID, email address, and office number
--- index.js
+++ index.js
@@ -1,1 +1,128 @@
+const inquirer = require("inquirer");
+const manager = require("./lib/Manager");
 // prompts for name id email role and role item
+// WHEN I start the application
+// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+// WHEN I enter the team manager’s name, employee ID, email address, and office number
+// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+// WHEN I select the engineer option
+// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+// WHEN I select the intern option
+// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+// WHEN I decide to finish building my team
+// THEN I exit the application, and the HTML is generated
+
+function askForManagerInfo() {
+    const managerInfo = inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the manager's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the manager's id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the manager's email?"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the manager's office number?"
+        }
+    ])
+    // TODO cr
+    addEngineerOrIntern()
+}
+
+function askForEngineerInfo() {
+    const engineerInfo = inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the engineer's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the engineer's id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the engineer's email?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is the engineer's github username?"
+        }
+    ])
+}
+
+function askForInternInfo() {
+    const internInfo = inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the intern's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the intern's id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the intern's email?"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is the intern's school?"
+        }
+    ])
+}
+
+function addEngineerOrIntern() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "Would you like to add an engineer or intern?",
+            choices: ["engineer", "intern"]
+        }
+    ])
+    .then((val) => {
+        if (val.role === "engineer") {
+            askForEngineerInfo()
+        } else {
+            askForInternInfo()
+        }
+
+    })
+}
+
+function isTeamComplete() {
+    inquirer.prompt([{
+        type: "confirm",
+        name: "complete",
+        message: "Is your team complete?"
+    }
+    ])
+    .then((val) => {
+        if (val.complete) {
+            // template function
+        } else {
+            addEngineerOrIntern()
+        }
+
+    })
+}
+
+askForManagerInfo()
