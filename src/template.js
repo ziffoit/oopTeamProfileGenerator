cb180a29034762eafe740ff86f4ce9392038dc23
Kevin Hanson <ziffoit@gmail.com>
2021-10-16 17:09:05 -0600

    bootstrap added for polish

--- src/template.js
+++ src/template.js
@@ -2,22 +2,26 @@
 
 const generateTeam = (team) => {
     const generateEmployee = (employee, specialItem) => {
-        return `<div class="card" style="width: 18rem;">
-        <div class="card-header">
-          <div>
-            ${employee.getName()}
-          </div>
-          <div>
-            ${employee.getRole()}
-          </div>
-            
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">ID: ${employee.getId()}</li>
-          <li class="list-group-item">Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a></li>
-          <li class="list-group-item">${specialItem}</li>
-        </ul>
-      </div>`;
+        return `
+        <div class="col">
+            <div class="card rounded-3">
+                <div class="card-header bg-info">
+                    <h2 class="card-title">
+                        ${employee.getName()}
+                    </h2>
+                    <h3 class="card-subtitle">    
+                        ${employee.getRole()}
+                    </h3>
+                </div>
+                <div class="card-body bg-light">
+                    <ul class="list-group list-group-flush">
+                    <li class="list-group-item border my-0 p-2">ID: ${employee.getId()}</li>
+                    <li class="list-group-item border my-0 p-2">Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a></li>
+                    <li class="list-group-item border my-0 p-2">${specialItem}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>`;
     };
     const generateManager = (manager) => {
         return generateEmployee(
@@ -26,7 +30,10 @@ const generateTeam = (team) => {
         );
     };
     const generateEngineer = (engineer) => {
-        return generateEmployee(engineer, `GitHub: <a target="_blank" href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a>`);
+        return generateEmployee(
+            engineer,
+            `GitHub: <a target="_blank" href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a>`
+        );
     };
     const generateIntern = (intern) => {
         return generateEmployee(intern, `School: ${intern.getSchool()}`);
@@ -53,7 +60,7 @@ const generateTeam = (team) => {
     );
 
     const body = html.join("");
-        return `<!DOCTYPE html>
+    return `<!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
@@ -62,9 +69,16 @@ const generateTeam = (team) => {
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
         </head>
         <body>
-            ${body}
+            <header>
+                <h1 style="text-align: center;" class="text-light bg-danger py-4">My Team</h1>
+            </header>
+            <main class="container my-5">
+                <div class="row row-cols-3 justify-content-center g-3">
+                    ${body}
+                </div>    
+            </main>    
         </body>
-        </html>`
+        </html>`;
 };
 
 module.exports = generateTeam;
